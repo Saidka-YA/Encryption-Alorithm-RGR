e7206a90693da4779211b1434dbd81a1f426e8f7
archRDS <2007dima1611@gmail.com>
2026-06-08 21:54:47 +0400

    добавлены реализации координатных функций

--- hex_utils.h
+++ hex_utils.h
@@ -1,8 +1,15 @@
 #pragma once
 #include <string>
+#include <utility>
 
 // Перевод байта в Hex-строку
 std::string byteToHex(unsigned char byte);
 
 // Перевод Hex-строки в байт
 unsigned char hexToByte(const std::string& hex);
+
+// Получение координат в сетке 4x4 для Hex-символа
+std::pair<int, int> hexDigitToCoords(char hexDigit);
+
+// Получение Hex-символа из координат сетки 4x4
+char coordsToHexDigit(int row, int col);
