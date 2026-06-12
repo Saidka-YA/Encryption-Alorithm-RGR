4eb2537f398b9c949be4d1c8cbdc44afe30eb301
NesterOV <ivannesterov200514@gmail.com>
2026-06-12 14:39:10 +0700

    Update textproc.cpp

--- textproc.cpp
+++ textproc.cpp
@@ -1,6 +1,6 @@
 #include "textproc.h"
 
-// Прямое преобразование: каждый байт строки становится числом 0-255
+// каждый байт строки становится числом 0-255
 vector<char32_t> to_codes(const string& str) {
     vector<char32_t> codes;
     for (unsigned char c : str) {
@@ -9,7 +9,7 @@ vector<char32_t> to_codes(const string& str) {
     return codes;
 }
 
-// Обратное преобразование: берем младшие 8 бит каждого кода
+// берем младшие 8 бит каждого кода
 string to_text(const vector<char32_t>& codes) {
     string result;
     for (char32_t cp : codes) {
